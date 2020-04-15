e338770ccf682f4e0b486470b77ce6aac0601aee
kullarat <anislove19@gmail.com>
2020-04-15 23:47:19 +0700

    git push origin master

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        return view('foods.main');
+    }
+
+    public function about()
+    {
+        return view('foods.about');
+    }
+    
+    public function contact()
+    {
+        return view('foods.contact');
+    }
+
+    public function article()
+    {
+        return view('foods.article');
+    }
+
+    public function disease()
+    {
+        return view('foods.disease');
+    }
+
+    public function profile()
+    {
+        return view('foods.profile');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('welcome', 'WelcomeController@index');
+
+Route::get('about', 'WelcomeController@about');
+
+Route::get('contact', 'WelcomeController@contact');
+
+Route::get('article', 'WelcomeController@article');
+
+Route::get('disease', 'WelcomeController@disease');
+
+Route::get('profile', 'WelcomeController@profile');
