aad21a6ff930a20bb93943bda93f92a181a6da27
kullarat <anislove19@gmail.com>
2020-05-03 15:47:12 +0700

    อัลบั้มรูปพร้อมชื่ออาหาร

--- resources/views/foods/gallery.blade.php
+++ resources/views/foods/gallery.blade.php
@@ -132,76 +132,91 @@
 				<div class="row">
 					<div class="col-sm-12 col-md-3 col-lg-4">
 						<a class="lightbox" href="images/img-01.jpg"> <!--ขยาย-->
+							<h1 style="text-align: center" >ข้าวราดต้มจืดเต้าหู้หมูสับ</h1>
 							<img class="img-fluid" src="images/img-01.jpg" alt="Gallery Images"> <!--ย่อ-->
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-02.jpg">
+							<h1 style="text-align: center" >ข้าวราดไข่เจียวสมุนไพร</h1>
 							<img class="img-fluid" src="images/img-02.jpg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-03.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงส้มชะอมทอด</h1>
 							<img class="img-fluid" src="images/img-03.jpg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-12 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-04.jpg">
+							<h1 style="text-align: center" >ข้าวราดผัดผักรวม</h1>
 							<img class="img-fluid" src="images/img-04.jpg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-05.jpg">
+							<h1 style="text-align: center" >ข้าวราดยำไข่าว</h1>
 							<img class="img-fluid" src="images/img-05.jpg" alt="Gallery Images">
 						</a>
 					</div> 
 					<div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-06.jpg">
+							<h1 style="text-align: center" >น้ำพริกกะปิ + ปลาทูทอด + ชะอมชุบไข่</h1>
 							<img class="img-fluid" src="images/img-06.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-07.jpg">
+							<h1 style="text-align: center" >ก๋วยเตี๋ยวเส้นหมี่เนื้อรวม น้ำตก</h1>
 							<img class="img-fluid" src="images/img-07.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-08.jpg">
+							<h1 style="text-align: center" >กระเพาะปลาเส้นหมี่</h1>
 							<img class="img-fluid" src="images/img-08.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-09.jpg">
+							<h1 style="text-align: center" >สุกี้รวมมิตร แห้ง</h1>
 							<img class="img-fluid" src="images/img-09.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-10.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงขี้เหล็ก</h1>
 							<img class="img-fluid" src="images/img-10.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-011.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงเขียวหวานไก่</h1>
 							<img class="img-fluid" src="images/img-011.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-012.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงคั่วกลิ้งหมู</h1>
 							<img class="img-fluid" src="images/img-012.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-013.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงฉู่ฉี่ปลาทู</h1>
 							<img class="img-fluid" src="images/img-013.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-014.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงไตปลา</h1>
 							<img class="img-fluid" src="images/img-014.jpg" alt="Gallery Images">
 						</a>
 					</div>
                     <div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/img-015.jpg">
+							<h1 style="text-align: center" >ข้าวราดแกงเทโพหมูสามชั้น</h1>
 							<img class="img-fluid" src="images/img-015.jpg" alt="Gallery Images">
 						</a>
 					</div>
